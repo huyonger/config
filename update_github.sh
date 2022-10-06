b9e4d5f6a07601fb09163b6e58c0f6ff86f9f2da
YongHu <2385410383@qq.com>
2022-10-06 23:05:26 +0800

    fix(update_github): change update_github curl command

--- update_github.sh
+++ update_github.sh
@@ -12,7 +12,8 @@ curl 'https://raw.hellogithub.com/hosts' \
   -H 'Sec-Fetch-Site: none' \
   -H 'Sec-Fetch-User: ?1' \
   -H 'Upgrade-Insecure-Requests: 1' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' \
-  -H 'sec-ch-ua: "Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"' \
   -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "macOS"' \ >> /etc/hosts
+  -H 'sec-ch-ua-platform: "macOS"' \
+  --compressed >> /etc/hosts
